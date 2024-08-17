8cb5b7ca3a2bcb8874c6b63d55451e93fe8e5fd9
satish980 <isatishkumar4008@gmail.com>
2024-08-17 20:03:32 +0530

    added close icon in widget card

--- src/components/WidgetCard.tsx
+++ src/components/WidgetCard.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { faClose } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { Category, Widget } from "../types";
 
 type Props = {
@@ -13,12 +16,12 @@ const WidgetCard: React.FC<Props> = ({ widget, removeWidget, category }) => {
       key={widget.id}
       className="relative p-6 bg-white rounded-lg shadow-md border border-gray-200 flex flex-col justify-between"
     >
-      <button
-        className="absolute top-0 right-1 text-red-500 hover:text-red-700 cursor-pointer"
+      <div
+        className="absolute top-0 right-1 text-gray-500 hover:text-blue-700 cursor-pointer"
         onClick={() => removeWidget(category.id, widget.id)}
       >
-        ✕
-      </button>
+        <FontAwesomeIcon icon={faClose} />
+      </div>
       <div>
         <h3 className="text-lg font-semibold break-words">{widget.name}</h3>
         <p className="text-gray-600 mt-2 break-words">{widget.content}</p>
